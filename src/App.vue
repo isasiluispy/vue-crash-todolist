e97562a2f9328ebe45dfec616bb4da125da1fd15
isasiluispy <isasiluispy@gmail.com>
2019-10-06 11:29:06 -0300

    use of vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,17 @@
 <template>
   <div id="app">
-    <Header/>
-    <AddTodo v-on:add-todo="addTodo"/>
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+    <Header />
+    <router-view/>
   </div>
 </template>
 
 <script>
   import Header from "./components/layout/Header";
-  import Todos from "./components/Todos";
-  import AddTodo from "./components/AddTodo";
-  import axios from 'axios';
-
 
   export default {
     name: 'app',
     components: {
-      Todos,
-      Header,
-      AddTodo
-    },
-    methods: {
-      deleteTodo(id) {
-
-        axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-          .then(() => this.todos = this.todos.filter(todo => todo.id !== id))
-          // eslint-disable-next-line no-console
-          .catch(err => console.log(err))
-      },
-      addTodo(newTodo) {
-        axios.post('https://jsonplaceholder.typicode.com/todos', newTodo)
-          .then(res => this.todos = [...this.todos, res.data])
-          // eslint-disable-next-line no-console
-          .catch(err => console.log(err));
-      }
-    },
-    data() {
-      return {
-        todos: []
-      }
-    },
-    created() {
-      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
-        .then(res => this.todos = res.data)
-        // eslint-disable-next-line no-console
-        .catch(err => console.log(err))
+      Header
     }
   }
 </script>
